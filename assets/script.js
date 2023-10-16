1dcaa2a25a12c0744852ad994ce61940691bcdef
DBrush <coldnebraska@gmail.com>
2023-10-16 18:27:09 -0500

    added save button function

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
-$(function () {
-  // Current date variables
-  let date = new Date()
-  let hour = date.getHours() // 24 hr format
-  let dayName = date.getDay() // returns as number index
-  let day = date.getDate()
-  let month = date.getMonth() // returns as number index
-  let newHour = hour - 12 // 12 hr format
+// Current date variables
+let date = new Date()
+let hour = date.getHours() // 24 hr format
+let dayName = date.getDay() // returns as number index
+let day = date.getDate()
+let month = date.getMonth() // returns as number index
+let newHour = hour - 12 // 12 hr format
+let button = $("button[data-index]")
 
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,58 +15,80 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   
+  button.on("click", function() {
+    console.log(this)
+  })
 
   // !change block colors based on current hour
+  setBlockColors()
+
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  getSchedule()
+
+  // !Current date code
+  getCurrentDate()
+});
+
+function setEvent() {
+  console.log("button clicked")
+}
+
+function setBlockColors() {
   // Change to current
   let currentHour = "hour-" + hour // 12 hr format
   let currentMilHour = "hour-" + newHour // 24 hr format
-
+ 
   if (hour > 12) {
     $("#" + currentMilHour).addClass("present")
-    console.log(currentMilHour + " changed to present")
+    // console.log(currentMilHour + " changed to present")
   } else {
     $("#" + currentHour).addClass("present")
-    console.log(currentHour + " changed to present")
+    // console.log(currentHour + " changed to present")
   }
-
+ 
   // Change to past
   for (i = 9; i < hour && i <= 12; i++) {
     let pastHours = "hour-" + i
-    console.log(pastHours + " changed to past")
+    // console.log(pastHours + " changed to past")
     $("#" + pastHours).addClass("past")
   }
   for (i = 1; i < newHour; i++) {
     let pastHours = "hour-" + i
-    console.log(pastHours + " changed to past")
+    // console.log(pastHours + " changed to past")
     $("#" + pastHours).addClass("past")
   }
-
+ 
   // Change to future
   if (hour > 12) {
     for (i = newHour + 1; i > newHour && i < 6; i++) {
       let futureHours = "hour-" + i // 
-      console.log(futureHours + " changed to future")
+      // console.log(futureHours + " changed to future")
       $("#" + futureHours).addClass("future")
     }
   } else {
     for (i = hour + 1; i > hour && i <= 12; i++) {
       let futureHours = "hour-" + i
-      console.log(futureHours + " changed to future")
+      // console.log(futureHours + " changed to future")
       $("#" + futureHours).addClass("future")
     }
     for (i = 1; i < 6; i++) {
       let futureHours = "hour-" + i
-      console.log(futureHours + " changed to future")
+      // console.log(futureHours + " changed to future")
       $("#" + futureHours).addClass("future")
-    }
-  }
+   }
+ }
+}
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+function getSchedule() {
+  let events = JSON.parse(localStorage.getItem("events"))
+  // console.log(events)
+
+
+}
   
-
-  // !Current date code
+function getCurrentDate() {
   let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   let suffix = ""
@@ -81,4 +104,4 @@ $(function () {
   }
   currentDay = days[dayName] + ", " + months[month] + " " + day + suffix
   $("#currentDay").text(currentDay)
-});
+}
